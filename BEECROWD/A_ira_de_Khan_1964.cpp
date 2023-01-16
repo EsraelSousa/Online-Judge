df14e7f9a14df051c1e226b878032351c4d1d091
Esrael Sousa <esraelsousa22@gmail.com>
2023-01-16 12:55:04 -0300

    concerto pasta CSES

---
+++ BEECROWD/A_ira_de_Khan_1964.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+
+int main(){
+    ios::sync_with_stdio(0); cin.tie(nullptr); cout.tie(nullptr);
+    ll C, Va, Vb, T, I, Ana, Bia;
+    cin >> C >> Va >> Vb >> T >> I;
+    C *= 100;
+    I *= 100;
+    T *= 60;
+    Ana = (T*Va)%C;
+    Bia = (T*Vb)%C;
+    cout << Ana << ' ' << Bia << '\n';
+    return 0;
+}
+
+/*
+500 100 200 20 300
+*/
