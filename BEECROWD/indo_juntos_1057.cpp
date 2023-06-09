a53f3f3fcf71ce8d6ebe57b2728321b8a62967bd
EsraelSousa <esraelsousa22@gmail.com>
2023-06-08 22:41:22 -0300

    add solution

--- BEECROWD/indo_juntos_1057.cpp
+++ BEECROWD/indo_juntos_1057.cpp
@@ -16,131 +16,137 @@ typedef vector<vi> vvi;
 
 const int MAXN = 15;
 
-struct Data{
+struct state{
 	int distancia = 0;
-	char grid[MAXN][MAXN];
-	vector<ii> play;
-	
-	bool operator<(const Data& o) const {
-        return this->distancia < o.distancia;
+	ii roboA, roboB, roboC;
+    
+    // construtores
+    state(ii A, ii B, ii C, int distancia){
+        this->roboA = A;
+        this->roboB = B;
+        this->roboC = C;
+        this->distancia = distancia;
     }
 
-    bool operator>(const Data& o) const {
-        return this->distancia > o.distancia;
+    state(){
+
     }
 
-    bool operator<=(const Data& o) const {
-        return this->distancia <= o.distancia;
-    }
-
-    bool operator>=(const Data& o) const {
-        return this->distancia >= o.distancia;
+    bool operator>(const state& other) const{
+        return this->distancia < other.distancia;
     }
 };
 
-bool isAlvo[MAXN][MAXN], vis[3][MAXN][MAXN];
-int n;
+char grid[MAXN][MAXN];
+int visitado[MAXN][MAXN][MAXN][MAXN][MAXN][MAXN];
+int n, ans;
 int dx[] = {-1, 1, 0, 0}, dy[] = {0, 0, -1, 1};
-set<string> visitado;
-int ordem[9][3] = {{0, 1, 2}, {0, 2, 1}, {1, 0, 2}, {1, 2, 0}, {2, 0, 1}, {2, 1, 0}, {0, 0, 0}, {1, 1, 1}, {2, 2, 2}};
 
-void reset(){
-	memset(isAlvo, 0, sizeof(isAlvo));
-	memset(vis, 0, sizeof(vis));
-    visitado.clear();
+bool isValido(ii robo){
+	if(robo.fs < 1 || robo.fs > n || robo.sd < 1 || robo.sd > n) return false;
+    return grid[ robo.fs ][ robo.sd ] != '#';
 }
 
-bool isVisitado(Data& estado){
-    string aux;
-    for(int i=1; i<=n; i++){
-        for(int j=1; j<=n; j++){
-            aux.push_back(estado.grid[i][j]);
-        }
-    }
-	return (visitado.find(aux) != visitado.end());
+bool isAlvo(state& x){
+    return grid[x.roboA.fs][x.roboA.sd] == 'X' &&
+           grid[x.roboB.fs][x.roboB.sd] == 'X' &&
+           grid[x.roboC.fs][x.roboC.sd] == 'X';
 }
 
-void addSet(Data& estado){
-    string aux;
-    for(int i=1; i<=n; i++)
-        for(int j=1; j<=n; j++)
-            aux.push_back(estado.grid[i][j]);
-    visitado.insert(aux);
+bool isDiferentes(ii A, ii B, ii C){
+    return A != B && A != C && B != C;
 }
 
-bool isBorda(int x, int y){
-    if(x < 1 || x > n || y < 1 || y > n) return 1;
-    return 0;
+bool isVisitado(state& x){
+    if(!visitado[ x.roboA.fs ][ x.roboA.sd ][ x.roboB.fs ][ x.roboB.sd ][ x.roboC.fs ][ x.roboC.sd ] && 
+                        isDiferentes(x.roboA, x.roboB, x.roboC))
+        return (visitado[ x.roboA.fs ][ x.roboA.sd ][ x.roboB.fs ][ x.roboB.sd ][ x.roboC.fs ][ x.roboC.sd ] = true);
+    return false;
 }
 
-bool isValido(int x, int y, Data& estado){
-	if(x < 1 || x > n || y < 1 || y > n) return 0;
-    //cout << isVisitado(estado) << ' ' << ((estado.grid[x][y] == '.')) << " in valida\n";
-	return (isVisitado(estado) && estado.grid[x][y] == '.');
-}
-
-int dijk(Data& estado){
-	priority_queue<Data, vector<Data>, greater<Data>> fila;
-	fila.push(estado);
+int dijk(state& estado){
+	priority_queue<state, vector<state>, greater<state>> fila;
+	ii newA, newB, newC;
+    bool hasA, hasB, hasC;
+    fila.push(estado);
+    visitado[ estado.roboA.fs ][ estado.roboA.sd ][ estado.roboB.fs ][ estado.roboB.sd ][ estado.roboC.fs ][ estado.roboC.sd ] = true;
+    state next;
 	while(fila.size()){
-        //cout << "entra fila\n";
 		estado = fila.top();
-		fila.pop();
-		if(isVisitado(estado) == true) continue;
-		//cout << estado.distancia << '\n';
-		if(isAlvo[ estado.play[0].fs ][ estado.play[0].sd ] && 
-           isAlvo[ estado.play[1].fs ][ estado.play[1].sd ] && 
-           isAlvo[ estado.play[2].fs ][ estado.play[2].sd ]) 
-                return estado.distancia;
+        fila.pop();
 
-		addSet(estado);
-		//cout << fila.size() << " tamanho ao entrar\n";
-		for(int i=0; i<4; i++){
-			for(int k=0; k<9; k++){
-                Data aux = estado;
-                for(int j=0; j<3; j++){
-                    int x = aux.play[ ordem[k][j] ].fs;
-                    int y = aux.play[ ordem[k][j] ].sd;
-                    if(isBorda(x+dx[i], y+dy[i]) || (aux.grid[ x + dx[i]][ y+dy[i] ] != '.')) continue; // nao anda o robo
-                    aux.play[ ordem[k][j] ].fs += dx[i];
-                    aux.play[ ordem[k][j] ].sd += dy[i];
-                    swap(aux.grid[x][y], aux.grid[ x + dx[i] ][ y + dy[i] ]);
+        if(isAlvo(estado)) return estado.distancia;
+
+        for(int i=0; i<4; i++){
+            newA.fs = estado.roboA.fs + dx[i], newA.sd = estado.roboA.sd + dy[i];
+            newB.fs = estado.roboB.fs + dx[i], newB.sd = estado.roboB.sd + dy[i];
+            newC.fs = estado.roboC.fs + dx[i], newC.sd = estado.roboC.sd + dy[i];
+
+            hasA = isValido(newA);
+            hasB = isValido(newB);
+            hasC = isValido(newC);
+
+            if(hasA || hasB || hasC){ // algum robo se movimenta
+                if(hasA){
+                    if(!hasB && !hasC && newA != estado.roboB && newA != estado.roboC) next.roboA = newA;
+                    else if(hasB && !hasC && newA != estado.roboC) next.roboA = newA;
+                    else if(hasC && !hasB && newA != estado.roboB) next.roboA = newA;
+                    else if(hasB && hasC) next.roboA = newA;
+                    else next.roboA = estado.roboA;
                 }
-                aux.distancia++;
-                if(isVisitado(aux) == false){
-                    //cout << aux.distancia << " add\n";
-                    fila.push(aux);
+                else next.roboA = estado.roboA;
+
+                if(hasB){
+                    if(!hasA && !hasC && newB != estado.roboA && newB != estado.roboC) next.roboB = newB;
+                    else if(hasA && !hasC && newB != estado.roboC) next.roboB = newB;
+                    else if(hasC && !hasA && newB != estado.roboA) next.roboB = newB;
+                    else if(hasA && hasC) next.roboB = newB;
+                    else next.roboB = estado.roboB;
                 }
+                else next.roboB = estado.roboB;
+
+                if(hasC){
+                    if(!hasA && !hasB && newC != estado.roboB && newC != estado.roboA) next.roboC = newC;
+                    else if(hasA && !hasB && newC != estado.roboB) next.roboC = newC;
+                    else if(hasB && !hasA && newC != estado.roboA) next.roboC = newC;
+                    else if(hasA && hasB) next.roboC = newC;
+                    else next.roboC = estado.roboC;
+                }
+                else next.roboC = estado.roboC;
+
+                next.distancia = estado.distancia + 1;
+                if(isVisitado(next)) fila.push(next);
             }
-		}
-        //cout << fila.size() << " tamanho ao sair\n";
+        }
+		
 	}
 	return -1;
 }
 
 void solve(){
     cin >> n;
-    reset();
-    Data inicio;
-    for(int i=1; i<=n; i++){
+    ii A, B, C;
+    for(int i=1; i<=n; i++)
         for(int j=1; j<=n; j++){
-            cin >> inicio.grid[i][j];
-            if(inicio.grid[i][j] >= 'A' &&  inicio.grid[i][j] <= 'C'){
-                inicio.play.push_back({i, j});
-            }
-            else if(inicio.grid[i][j] == 'X'){
-            	isAlvo[i][j] = 1;
-            	inicio.grid[i][j] = '.';
-            }
+            cin >> grid[i][j];
+            if(grid[i][j] == 'A')
+                A = ii(i, j);
+            if(grid[i][j] == 'B')
+                B = ii(i, j);
+            if(grid[i][j] == 'C')
+                C = ii(i, j);
         }
-    }
-    int ans = dijk(inicio);
-    if(ans == -1) cout << "trapped\n";
+    
+    memset(visitado, 0, sizeof(visitado));
+    ans = 1e9;
+    state inicio(A, B, C, 0);
+    dijk(inicio);
+    if(ans == (int)1e9) cout << "trapped\n";
     else cout << ans << '\n';
 }
 
 int main(){
-    ios_base::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL);
+    fast
     int t, id=1;
     cin >> t;
     while(t--){
