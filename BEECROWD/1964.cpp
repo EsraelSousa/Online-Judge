b25939bac9e0e58d8dabbf5abbcc791f8c3a6c3e
EsraelSousa <esraelsousa22@gmail.com>
2023-07-31 14:06:12 -0300

    add solution

--- BEECROWD/1964.cpp
+++ BEECROWD/1964.cpp
@@ -7,14 +7,19 @@ typedef long long ll;
 
 int main(){
     ios::sync_with_stdio(0); cin.tie(nullptr); cout.tie(nullptr);
-    ll C, Va, Vb, T, I, Ana, Bia;
+    ll C, Va, Vb, T, I, Ana = 0, Bia = 0;
     cin >> C >> Va >> Vb >> T >> I;
     C *= 100;
     I *= 100;
     T *= 60;
-    Ana = (I+T*Va)%C;
-    Bia = (I+T*Vb)%C;
-    cout << Ana << ' ' << Bia << '\n';
+    Ana = (I + ((T*Va)))%C; // posição de Ana no tempo T-1
+    Bia = (I + ((T*Vb)))%C; // posição de BIa no tempo T-1
+    //cout << Ana << ' ' << Bia << '\n';
+    //cout << fixed << setprecision(12) << 1.0 * (C-Ana) / Va << ' ' << 1.0 * (C-Bia) / Vb << ' ' << (1.0 * (C-Ana) / Va) - (1.0 * (C-Bia) / Vb) <<'\n';
+    if(Ana == 0 && Bia == 0) cout << ((Va >= Vb)? "Ana\n" : "Bia\n");
+    //else if(Ana == 0) cout << "Ana\n";
+    //else if(Bia == 0 ) cout << "Bia\n";
+    else cout << ((1.0 * (C-Ana) / Va <= 1.0 * (C-Bia) / Vb)? "Ana\n" : "Bia\n");
     return 0;
 }
 
---
+++ BEECROWD/Letras_1724.cpp
@@ -0,0 +1,110 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fast ios::sync_with_stdio(0); cin.tie(nullptr); cout.tie(nullptr);
+typedef long long ll;
+typedef unsigned long long ull;
+typedef vector<int> vi;
+typedef pair<int, int> ii;
+typedef vector<vi> vvi;
+#define ff first
+#define ss second
+#define all(x) x.begin(), x.end()
+#define sz(x) (int)x.size()
+#define left(x) (2*x)
+#define right(x) (2*x + 1)
+
+const int MAXN = 1e2+5;
+
+struct Data{
+	int x;
+	int y;
+    int dist;
+	int maskLower;
+	int maskUpper;
+	
+	Data(){
+		
+	}
+	
+	Data(int _x, int _y, int _dist, int maskL, int maskU) : x(_x), y(_y), dist(_dist), maskLower(maskL), maskUpper(maskU){
+	
+	}
+};
+
+int n;
+char mat[MAXN][MAXN];
+int vis[MAXN][MAXN][1 << 10];
+int vis1[MAXN][MAXN][1 << 10];
+int dx[] = {1, -1, 0, 0}, dy[] = {0, 0, 1, -1};
+
+int bfs(int x, int y){
+	queue<Data> fila;
+	int ans = 1e9;
+    int maskL = 0, maskU = 0;
+    if(islower(mat[x][y])) maskL |= 1 << (mat[x][y] - 'a');
+    else maskU |= 1 << (mat[x][y] - 'A');
+    //cout << maskL << ' ' << maskU << '\n';
+	fila.push(Data(x, y, 1, maskL, maskU));
+	while(sz(fila)){
+		x = fila.front().x;
+		y = fila.front().y;
+		int maskL = fila.front().maskLower;
+		int maskU = fila.front().maskUpper;
+		int d = fila.front().dist;
+		fila.pop();
+
+		if(vis[x][y][maskL] && vis1[x][y][maskU]) continue;
+		
+        if(x == n && y == n)
+            ans = min(ans, d);
+		vis[x][y][maskL] = vis1[x][y][maskU] = 1;
+		for(int i=0; i<4; i++){
+			int xx = x + dx[i];
+			int yy = y + dy[i];
+			int maskLo = maskL;
+			int maskUp = maskU;
+			if(xx >= 1 && xx <= n && yy >= 1 && yy <= n){
+				if(islower(mat[xx][yy])){
+					if((maskU & (1 << (mat[xx][yy] - 'a'))) != 0) continue;
+					maskLo |= 1 << (mat[xx][yy] - 'a');
+					fila.push(Data(xx, yy, d+1, maskLo, maskUp));
+				}
+				else{
+					if((maskL & (1 << (mat[xx][yy] - 'A'))) != 0) continue;
+					maskUp |= 1 << (mat[xx][yy] - 'A');
+					fila.push(Data(xx, yy, d+1, maskLo, maskUp));
+				}
+			}
+		}
+	}
+	return (ans == 1e9)? -1 : ans;
+}
+
+void task(){
+    cin >> n;
+	for(int i=1; i<=n; i++)
+		cin >> (mat[i]+1);
+
+    cout << bfs(1, 1) << '\n';
+}
+
+int main(){
+    fast
+    int tes = 1;
+    //cin >> tes;
+    while(tes--)
+        task();
+    return 0;
+}
+
+/*
+6
+DdaAaA
+CBAcca
+eEaeeE
+bBbabB
+DbDbDc
+fFaAaC
+
+*/
